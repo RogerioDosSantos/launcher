b888ccceec75594a37df3b675bc34641ac04f592
Roger <roger.santos.main@gmail.com>
2018-05-27 13:31:51 -0500

    Migrated help to documents

--- scripts/_doc.sh
+++ scripts/_doc.sh
@@ -3,5 +3,25 @@ doc::ShowMarkdown()
 {
   # Usage ShowMarkdown <markdown_file_path>
   local file_path=$1
-  pandoc "${file_path}" | lynx -dump -stdin
+  pandoc --wrap=preserve "${file_path}" | lynx -dump -stdin
 }
+
+doc::Help()
+{
+  # Usage DisplayHelp <in:file_path>
+  local file_path=$1
+
+  log::Log "info" "5" "Markdown File" "${file_path}"
+  if [ "${file_path}" == "" ]; then
+    doc::ShowMarkdown "../doc/src/index.md"
+    return 0
+  fi
+
+  if [ -f "${file_path}" ]; then
+    doc::ShowMarkdown "${file_path}"
+    return 0
+  fi
+
+  doc::ShowMarkdown "./_runner.md"
+}
+
