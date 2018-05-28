9bae22a341eef598021148007d3de1da16f3eb21
Roger <roger.santos.main@gmail.com>
2018-05-27 20:35:44 -0500

    Created GetDependencyFromConfig test

--- scripts/_script_tests.sh
+++ scripts/_script_tests.sh
@@ -1,8 +1,6 @@
 
 script_tests::GetDependencyFromConfig()
 {
-  script::GetDependencyFromConfig ../quality/script_tests-basic.dep
-
-  # json::VarsToJson pr1 pr2 pr3 pr4 | qa::AreEqual "4_assignments" "Wrong assignment"
+  script::GetDependencyFromConfig ../quality/script_tests-basic.dep | qa::AreEqual "basic_dependency_file" "Could not load a basic dependency file"
 }
 
