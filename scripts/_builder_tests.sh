ab22f198f18fc106fc64b089879b55886dc308a6
Roger <roger.santos.main@gmail.com>
2018-06-05 22:24:15 -0500

    Added validation of the launcher

--- scripts/_builder_tests.sh
+++ scripts/_builder_tests.sh
@@ -1,12 +1,13 @@
 
 builder_tests::CreateProjectMetadata()
 {
+  builder::CreateProjectMetadata "." | grep 'full_name' | qa::AreEqual "launcher_metadata" "Could not create the proper unique name"
   # builder::CreateProjectMetadata "~/indusoft/projects/hardware_validator"
   # builder::CreateProjectMetadata "~/indusoft/projects/historian"
   # builder::CreateProjectMetadata "~/indusoft/projects/zeromq"
   # builder::CreateProjectMetadata "~/indusoft/projects/serialization"
   # builder::CreateProjectMetadata "~/indusoft/projects/service"
-  builder::CreateProjectMetadata "~/indusoft/projects/third-party/curl"
+  # builder::CreateProjectMetadata "~/indusoft/projects/third-party/curl"
 }
 
 builder_tests::BuildCmake()
