ff0059dfd315f14e17b7345e69664e60e2a69c69
Roger <roger.santos.main@gmail.com>
2018-06-05 19:29:30 -0500

    Finished Project Metadata TODO: Add tags

--- scripts/_builder.sh
+++ scripts/_builder.sh
@@ -14,69 +14,41 @@ builder::CreateProjectMetadata()
     printf '\"%s\": \"%s\",' 'commit' \"\$(git rev-parse HEAD)\" | echo \$(cat) ;
     printf '\"%s\": \"%s\",' 'branch' \"\$(git rev-parse --abbrev-ref HEAD)\" | echo \$(cat) ;
     printf '\"%s\": \"%s\",' 'timestamp' \"\$(git log -1 --date=iso --pretty=format:%cd)\" | echo \$(cat) ;
+    # printf '\"%s\": \"%s\",' 'tag' \"\$(git describe --tags)\" | echo \$(cat) ;
     cd ..
     printf '\"%s\": \"%s\",' 'upper_module_dir' \"\$(git rev-parse --show-toplevel)\" | echo \$(cat) ;
     printf '\"%s\": \"%s\",' 'upper_version' \"\$(git rev-parse --short HEAD)\" | echo \$(cat) ;
     printf '\"%s\": \"%s\",' 'upper_commit' \"\$(git rev-parse HEAD)\" | echo \$(cat) ;
     printf '\"%s\": \"%s\",' 'upper_branch' \"\$(git rev-parse --abbrev-ref HEAD)\" | echo \$(cat) ;
     printf '\"%s\": \"%s\"' 'upper_timestamp' \"\$(git log -1 --date=iso --pretty=format:%cd)\" | echo \$(cat) ;
+    # printf '\"%s\": \"%s\",' 'upper_tag' \"\$(git describe --tags)\" | echo \$(cat) ;
     echo '}'
   ")"
 
-  local t1="$(echo "${git_result}" | python -c "import sys, json; print json.load(sys.stdin)['version']")"
-
-
-
-
-  echo "${t1}"
-  return 0
-
-  local module_dir="$(script::ExecOnHost "false" "cd ${in_project_dir} ; git rev-parse --show-toplevel")"
   local name="${module_dir##*/}"
-  local version="$(script::ExecOnHost "false" "cd ${in_project_dir} ; git rev-parse --short HEAD")"
-  local commit="$(script::ExecOnHost "false" "cd ${in_project_dir} ; git rev-parse HEAD")"
-  local branch="$(script::ExecOnHost "false" "cd ${in_project_dir} ; git rev-parse --abbrev-ref HEAD")"
-  local timestamp="$(script::ExecOnHost "false" "cd ${in_project_dir} ; git log -1 --date=iso --pretty=format:%cd")"
-
-  local upper_module_dir="$(script::ExecOnHost "false" "cd ${module_dir}/.. ; git rev-parse --show-toplevel")"
+  local version="$(json::GetValue "${git_result}" 'version')"
+  local commit="$(json::GetValue "${git_result}" 'commit')"
+  local branch="$(json::GetValue "${git_result}" 'branch')"
+  local timestamp="$(json::GetValue "${git_result}" 'timestamp')"
+  local tag="$(json::GetValue "${git_result}" 'tag')"
+  local upper_module_dir="$(json::GetValue "${git_result}" 'upper_module_dir')"
   local upper_name="${upper_module_dir##*/}"
-  local upper_version="$(script::ExecOnHost "false" "cd ${module_dir}/.. ; git rev-parse --short HEAD")"
-  local upper_commit="$(script::ExecOnHost "false" "cd ${module_dir}/.. ; git rev-parse HEAD")"
-  local upper_branch="$(script::ExecOnHost "false" "cd ${module_dir}/.. ; git rev-parse --abbrev-ref HEAD")"
-  local upper_timestamp="$(script::ExecOnHost "false" "cd ${module_dir}/.. ; git log -1 --date=iso --pretty=format:%cd")"
+  local upper_version="$(json::GetValue "${git_result}" 'upper_version')"
+  local upper_commit="$(json::GetValue "${git_result}" 'upper_commit')"
+  local upper_branch="$(json::GetValue "${git_result}" 'upper_branch')"
+  local upper_timestamp="$(json::GetValue "${git_result}" 'upper_timestamp')"
+  local upper_tag="$(json::GetValue "${git_result}" 'upper_tag')"
 
-
-  local upper_location="${module_dir/${upper_module_dir}/}"
-
-  local location="${config_location/${module_dir}/}"
-  local full_name="${upper_name}-${name}-${location}"
+  local location="${module_dir/${upper_module_dir}/}"
+  location="${location/${name}/}"
+  location="${location::-1}"
+  local full_name="${upper_name}-${location}-${name}"
   local full_version="${upper_branch}-${upper_version}-${branch}-${version}"
-  full_name=${full_name/\//-}
+  full_name=${full_name//\//-}
   full_name=${full_name/--/-}
   full_version=${full_version/--/-}
 
-  json_ret="{ 
-    "\"name\"":"\"${name}\"",
-    "\"version\"":"\"${version}\"",
-    "\"commit\"":"\"${commit}\"",
-    "\"branch\"":"\"${branch}\"",
-    "\"timestamp\"":"\"${timestamp}\"",
-    "\"location\"":"\"${location}\"",
-    "\"upperName\"":"\"${upper_name}\"",
-    "\"upperVersion\"":"\"${upper_version}\"",
-    "\"upperCommit\"":"\"${upper_commit}\"",
-    "\"upperBranch\"":"\"${upper_branch}\"",
-    "\"upperTimeStamp\"":"\"${upper_timestamp}\"",
-    "\"upperLocation\"":"\"${upper_location}\"",
-    "\"fullName\"":"\"${full_name}\"",
-    "\"fullVersion\"":"\"${full_version}\""
-
-    echo "${json_ret}"
-    if [ ${config_output_file_path} != "" ]; then 
-      cd "${g_caller_dir}"
-      echo "${json_ret}" > ${config_output_file_path}
-    fi
-  }"
+  json::VarsToJson name version commit branch timestamp tag location upper_name upper_version upper_commit upper_branch upper_timestamp upper_tag upper_location full_name full_version
 }
 
 builder::BuildCmake ()
--- scripts/_builder_tests.sh
+++ scripts/_builder_tests.sh
@@ -1,7 +1,12 @@
 
 builder_tests::CreateProjectMetadata()
 {
-  builder::CreateProjectMetadata "~/indusoft/projects/hardware_validator"
+  # builder::CreateProjectMetadata "~/indusoft/projects/hardware_validator"
+  # builder::CreateProjectMetadata "~/indusoft/projects/historian"
+  # builder::CreateProjectMetadata "~/indusoft/projects/zeromq"
+  # builder::CreateProjectMetadata "~/indusoft/projects/serialization"
+  # builder::CreateProjectMetadata "~/indusoft/projects/service"
+  builder::CreateProjectMetadata "~/indusoft/projects/third-party/curl"
 }
 
 builder_tests::BuildCmake()
